6dda74c76205eb697257c478b297a5eb138ef8ca
Cameron burridge <cmburridge4022@gmail.com>
2020-08-25 23:50:24 -0600

    Added basic movement

---
+++ 2670Project/Assets/C#/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 1;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float f = Input.GetAxisRaw("Vertical");
+        
+        
+        gameObject.transform.position = new Vector3(transform.position.x + (h * speed),transform.position.y,transform.position.z + (f * speed));
+    }
+}
