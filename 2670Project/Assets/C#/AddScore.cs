0fe9e41b07eac084bed3ea5d27653a2a880fbf3b
Cameron burridge <cmburridge4022@gmail.com>
2020-08-26 14:17:00 -0600

    In class IntData and Scriptable Objects

---
+++ 2670Project/Assets/C#/AddScore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddScore : MonoBehaviour
+{
+    public IntData scoreValue;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        scoreValue.value++;
+    }
+}
---
+++ 2670Project/Assets/C#/Score.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public IntData scoreValue;
+    private Text _text;
+
+    private void Start()
+    {
+        _text = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        _text.text = scoreValue.value.ToString();
+    }
+}
