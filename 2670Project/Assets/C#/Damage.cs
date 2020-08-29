222100a4a3ee4cfac749addb66db72df6a402f0a
Cameron burridge <cmburridge4022@gmail.com>
2020-08-28 22:51:40 -0600

    Added damaging enemy and player death

---
+++ 2670Project/Assets/C#/Damage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    public IntData healthValue;
+    public GameObject player;
+
+    private void Start()
+    {
+        healthValue.value = 3;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        healthValue.value--;
+    }
+
+    private void FixedUpdate()
+    {
+        if (healthValue.value <= 0)
+        {
+            Destroy(player);
+        }
+    }
+}
