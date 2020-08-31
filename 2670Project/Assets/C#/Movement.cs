83e38802a5c6b46b36b9b04e7e20621ae3e48dd8
Cameron burridge <cmburridge4022@gmail.com>
2020-08-31 14:37:33 -0600

    Inclass new movement. Out of class sprint function.

--- 2670Project/Assets/C#/Movement.cs
+++ 2670Project/Assets/C#/Movement.cs
@@ -1,24 +1,58 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class Movement : MonoBehaviour
 {
-    public float speed = 1;
-   
-    // Start is called before the first frame update
+    
+    private CharacterController controller;
+    private Vector3 _movement;
+    public float gravity = 9.81f;
+    public float moveSpeed = 3f;
+    public float fastSpeed = 6f;
+    public float jumpForce = 10f;
+    public int jumpCountMax;
+    public float rotateSpeed = 3f;
+    private Vector3 rotateMovement;
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        float h = Input.GetAxisRaw("Horizontal");
-        float f = Input.GetAxisRaw("Vertical");
-        
-        
-        gameObject.transform.position = new Vector3(transform.position.x + (h * speed),transform.position.y,transform.position.z + (f * speed));
+        rotateMovement.y = rotateSpeed * Input.GetAxis("Horizontal");
+        transform.Rotate(rotateMovement);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            _movement.y = jumpForce;
+        }
+
+        if (controller.isGrounded)
+        {
+            _movement.y = 0;
+        }
+        else
+        {
+            _movement.y -= gravity;
+        }
+
+        if (Input.GetButton("Fire1"))
+        {
+            _movement.x = Input.GetAxis("Horizontal")* fastSpeed;
+            _movement.z = Input.GetAxis("Vertical")* fastSpeed;
+        }
+        else
+        {
+            _movement.x = Input.GetAxis("Horizontal")* moveSpeed;
+            _movement.z = Input.GetAxis("Vertical")* moveSpeed;
+        }
+
+        _movement = transform.TransformDirection(_movement);
+        controller.Move(_movement*Time.deltaTime);
     }
 }
