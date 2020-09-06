5821f8170a5e65ed0efcbf8f2e4e3d6bc960641f
Cameron burridge <cmburridge4022@gmail.com>
2020-09-05 21:17:48 -0600

    Added set active for puzzles

---
+++ 2670Project/Assets/C#/Activate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activate : MonoBehaviour
+{
+    public GameObject puzzleObject;
+    private void OnTriggerEnter(Collider other)
+    {
+        puzzleObject.SetActive(true);
+    }
+}
