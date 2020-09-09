b5c3e9b2ba77a2a002f255682dfae1ce3a315d1d
Cameron burridge <cmburridge4022@gmail.com>
2020-09-09 14:19:42 -0600

    Added many data types and fixed jumps sprint and made a power up

--- 2670Project/Assets/C#/Movement.cs
+++ 2670Project/Assets/C#/Movement.cs
@@ -9,30 +9,41 @@ public class Movement : MonoBehaviour
     private CharacterController controller;
     private Vector3 movement;
 
-    public float moveSpeed = 5f, fastSpeed = 10f, rotateSpeed = 5f, gravity = -9.81f, jumpForce = 10f;
+    public float rotateSpeed = 5f, gravity = -9.81f, jumpForce = 10f;
     private float yVar;
 
-    public int jumpCountMax = 2;
+    public FloatData moveSpeed, fastSpeed;
+    public FloatData normalSpeed;
+
+    public Vector3Data currentSpawnPoint;
+    
+    public FloatData MoveSpeed => moveSpeed;
+
+    public IntData playerJumpCount;
     private int jumpCount;
     
     private void Start()
     {
+        moveSpeed = normalSpeed;
         controller = GetComponent<CharacterController>();
     }
 
     private void Update()
     {
-        if (Input.GetButton("Fire1"))
         {
-            var vInput = Input.GetAxis("Vertical")*fastSpeed;
-            movement.Set(vInput,yVar,0);
-        }
-        else
-        {
-            var vInput = Input.GetAxis("Vertical")*moveSpeed;
+            var vInput = Input.GetAxis("Vertical")*moveSpeed.value;
             movement.Set(vInput,yVar,0);
         }
         
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            moveSpeed = fastSpeed;
+        }
+        
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            moveSpeed = normalSpeed;
+        }
         
         var hInput = Input.GetAxis("Horizontal")* Time.deltaTime*rotateSpeed;
         transform.Rotate(0,hInput,0);
@@ -45,7 +56,7 @@ public class Movement : MonoBehaviour
             jumpCount = 0;
         }
 
-        if (Input.GetButtonDown("Jump") && jumpCount< jumpCountMax)
+        if (Input.GetButtonDown("Jump") && jumpCount< playerJumpCount.value)
         {
             yVar = jumpForce;
             jumpCount++;
@@ -53,6 +64,11 @@ public class Movement : MonoBehaviour
 
         movement = transform.TransformDirection(movement);
         controller.Move(movement * Time.deltaTime);
+    }
+
+    private void OnEnable()
+    {
+        //set the position of player to the location data of the player
     }
 }
 
---
+++ 2670Project/Assets/C#/SpawnPoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    public Vector3Data vData;
+
+    //Set vData from position value
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //set the location data of the player to the current spawnPoint
+    }
+}
