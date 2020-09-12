43e9e2a96506fdf6472413fb514343cee19a286e
Cameron burridge <cmburridge4022@gmail.com>
2020-09-11 21:31:30 -0600

    Created center rotating follow camera, need to swap out for cinimachine

---
+++ 2670Project/Assets/C#/LookAt.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    public Transform player;
+    
+    void Update()
+    {
+        transform.LookAt(player);
+    }
+}
---
+++ 2670Project/Assets/C#/RotateCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    public float rotateSpeed = 5f;
+    void Update()
+    {
+        var hInput = Input.GetAxis("Mouse X")* Time.deltaTime*rotateSpeed;
+        transform.Rotate(0,hInput,0);
+    }
+}
