035f8192c25f68c5ceaa9dc9f4d8f7efbd92b419
Cameron burridge <cmburridge4022@gmail.com>
2020-09-17 22:57:55 -0600

    Added dolly camera

---
+++ 2670Project/Assets/Follow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public GameObject followObj;
+    public Transform objLocation;
+    public float speed = 2.0f;
+
+    void Update()
+    {
+
+        if (Vector3.Distance(transform.position, objLocation.position) < 10f)
+        {
+           return; 
+        }
+        else
+        {
+             float interpolation = speed * Time.deltaTime;
+            
+             Vector3 position = this.transform.position;
+             position.y = Mathf.Lerp(this.transform.position.y, followObj.transform.position.y, interpolation);
+             position.x = Mathf.Lerp(this.transform.position.x, followObj.transform.position.x, interpolation);
+            
+             this.transform.position = position;
+        }
+       
+    }
+}
