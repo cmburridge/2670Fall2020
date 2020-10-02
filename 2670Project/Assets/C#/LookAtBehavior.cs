8ae673a79152a436f55fa01c905a6fd416303b1d
Cameron burridge <cmburridge4022@gmail.com>
2020-10-01 22:01:21 -0600

    Added new rotating guard enemy type

--- 2670Project/Assets/C#/LookAtBehavior.cs
+++ 2670Project/Assets/C#/LookAtBehavior.cs
@@ -2,24 +2,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class LookAtBehavior : MonoBehaviour
 {
     public float speed = 3f;
+    public float timer = 3f;
     public Transform objLocation;
     public bool enraged = false;
-
     public void LocatePlayer()
     {
         enraged = true;
     }
-
-     private void FixedUpdate()
+    
+    private void FixedUpdate()
         {
             if (enraged == true)
             {
                 transform.LookAt(objLocation);
-                transform.Translate(0,0,speed * Time.deltaTime);
+                transform.Translate(0,0,speed * Time.fixedDeltaTime);
+            }
+
+            if (enraged == false)
+            {
+               if ( this.tag == "Enemy")
+               {
+                   timer -= Time.fixedDeltaTime;
+                   if (timer <= 0f)
+                   {
+                       timer += 3f;
+                       transform.Rotate(0,90,0);
+                       Debug.Log("searching");
+                   }
+               } 
             }
         }
      
