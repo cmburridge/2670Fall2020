7c6618ae4a0184dfb3e02a4459460f1e29d3ee22
Cameron burridge <cmburridge4022@gmail.com>
2020-11-06 23:51:17 -0700

    Uploaded animations and created state machine

---
+++ 2670Project/Assets/C#/CharacterAnim.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnim : MonoBehaviour
+{
+   private Animator anim;
+
+   private void Start()
+   {
+      anim = GetComponent<Animator>();
+   }
+
+   private void Update()
+   {
+      if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+      {
+         anim.SetBool("Is running", true);
+      }
+      else
+      {
+         anim.SetBool("Is running", false);
+      }
+      
+      if (Input.GetKey
+         (KeyCode.UpArrow))
+      {
+         anim.SetBool("Is jumping", true);
+      }
+      else
+      {
+         anim.SetBool("Is jumping", false);
+      }
+   }
+}
